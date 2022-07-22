1af10d829eaed33901b8ed2d720044408219330b
Nazar <zvarych.nz@gmail.com>
2022-07-22 14:15:11 +0300

    first work on course

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict"
+
+let numberOfFilms = prompt("How many fils did u watch?", "");
+
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt("What was last film u watched?", ""),
+    b = prompt("How can u mark it?", ""),
+    c = prompt("What was last film u watched?", ""),
+    d = prompt("How can u mark it?", "");
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
