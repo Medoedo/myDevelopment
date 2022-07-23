3573b5ae3b22e46107847530e4a1f3a262e2125f
Nazar <zvarych.nz@gmail.com>
2022-07-23 11:56:13 +0300

    Third done task from course

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 "use strict";
 
-let numberOfFilms = prompt("How many fils did u watch?", "");
+let numberOfFilms;
 
-console.log(numberOfFilms);
+function start() {
+    numberOfFilms = prompt("How many films did u watch?", "");
+
+    while (numberOfFilms === "" || numberOfFilms === null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt("How many fils did u watch?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,50 +20,49 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count < 10) {
-    alert("Malo");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("Cool");
-} else if (personalMovieDB.count > 30) {
-    alert("Kinoman");
-}
-
-let a, b;
-
-for (let i = 0; i < 2; i++) {
-    a = prompt("What was last film u watched?", "");
-    b = prompt("How can u mark it?", "");
-
-    while (a === null || b === null || a === "" || b === "" || a.length > 50 || b.length > 50) {
-        a = prompt("What was last film u watched?", "");
-        b = prompt("How can u mark it?", ""); 
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Malo");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("Cool");
+    } else if (personalMovieDB.count > 30) {
+        alert("Kinoman");
     }
-
-    personalMovieDB.movies[a] = b;
 }
 
-<<<<<<< HEAD
-    console.log(personalMovieDB);
+detectPersonalLevel();
 
-console.log("just okay");
-=======
+function rememberMyFilms() {
+    let a, b;
+
+    for (let i = 0; i < 2; i++) {
+        a = prompt("What was last film u watched?", "");
+        b = prompt("How can u mark it?", "");
+
+        while (a === null || b === null || a === "" || b === "" || a.length > 50 || b.length > 50) {
+            a = prompt("What was last film u watched?", "");
+            b = prompt("How can u mark it?", ""); 
+        }
+        personalMovieDB.movies[a] = b;
+    }
+}
+
+rememberMyFilms();
+
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Your favorite genres under number ${i + 1}`);
+    }
+}
+
+writeYourGenres();
+
 console.log(personalMovieDB);
-
-
-    
-// const lines = 5;
-// let result = '';
-
-// for (let i = 0; i <= lines; i++) {
-
-//     for (let k = 0; k < lines - i; k++) {
-//         result += " ";
-//     }
-//     for (let j = 0; j < 1 + i * 2; j++) {
-//         result += "*";
-//     }
-
-//     result += "\n";
-// }
-// console.log(result);
->>>>>>> 8ee176c (new commit in new branch just for check)
