b70f34895c3405ba567e62572a447176a7f61fcd
Nazar <zvarych.nz@gmail.com>
2022-08-01 17:25:23 +0300

    saving all progress

--- js/script.js
+++ js/script.js
@@ -1,87 +1,80 @@
-"use strict";
+// "use strict";
 
-let numberOfFilms;
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        numberOfFilms = prompt("How many films did u watch?", "");
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = prompt("How many films did u watch?", "");
     
-        while (numberOfFilms === "" || numberOfFilms === null || isNaN(numberOfFilms)) {
-            numberOfFilms = prompt("How many fils did u watch?", "");
-        }
+//         while (personalMovieDB.count === "" || personalMovieDB.count === null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = prompt("How many fils did u watch?", "");
+//         }
+//     },
+//     detectPersonalLevel: function() {
+//         if (personalMovieDB.count < 10) {
+//             alert("Malo");
+//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//             alert("Cool");
+//         } else if (personalMovieDB.count > 30) {
+//             alert("Kinoman");
+//         }
+//     },
+//     rememberMyFilms: function() {
+//         let a, b;
     
-        personalMovieDB.count = numberOfFilms;
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            alert("Malo");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            alert("Cool");
-        } else if (personalMovieDB.count > 30) {
-            alert("Kinoman");
-        }
-    },
-    rememberMyFilms: function() {
-        let a, b;
+//         for (let i = 0; i < 2; i++) {
+//             a = prompt("What was last film u watched?", "");
+//             b = prompt("How can u mark it?", "");
     
-        for (let i = 0; i < 2; i++) {
-            a = prompt("What was last film u watched?", "");
-            b = prompt("How can u mark it?", "");
+//             while (a === null || b === null || a === "" || b === "" || a.length > 50 || b.length > 50 || isNaN(b)) {
+//                 a = prompt("What was last film u watched?", "");
+//                 b = prompt("How can u mark it?", ""); 
+//             }
+//             personalMovieDB.movies[a] = b;
+//         }
+//     },
+//     showMyDB: function() {
+//         if (personalMovieDB.privat === false) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     writeYourGenres: function() {
+//         for (let i = 0; i < 3; i++) {
+//             let a = prompt(`Your favorite genres under number ${i + 1}`, "");
+//             if (a == null || a === "") {
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres[i] = a;
+//             }
+//         }
     
-            while (a === null || b === null || a === "" || b === "" || a.length > 50 || b.length > 50 || isNaN(b)) {
-                a = prompt("What was last film u watched?", "");
-                b = prompt("How can u mark it?", ""); 
-            }
-            personalMovieDB.movies[a] = b;
-        }
-    },
-    showMyDB: function() {
-        if (personalMovieDB.privat === false) {
-            console.log(personalMovieDB);
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 0; i < 3; i++) {
-            let a = prompt(`Your favorite genres under number ${i + 1}`, "");
-            if (a == null || a === "") {
-                i--;
-            } else {
-                personalMovieDB.genres[i] = a;
-            }
-    
-            // while (a == null || a === "") {
-            //     a = prompt(`Your favorite genres under number ${i + 1}`, "").trim();
-            // } 
-        }
-    
-        personalMovieDB.genres.forEach((key, index) => {
-            console.log(`Людимый жанр #${index + 1} - это ${key}`);
-        });
-    },
-    toggleVisibleMyDB: function() {
-        personalMovieDB.privat === false ? personalMovieDB.privat = true : personalMovieDB.privat = false;
-    }
-};
+//         personalMovieDB.genres.forEach((key, index) => {
+//             console.log(`Людимый жанр #${index + 1} - это ${key}`);
+//         });
+//     },
+//     toggleVisibleMyDB: function() {
+//         personalMovieDB.privat === false ? personalMovieDB.privat = true : personalMovieDB.privat = false;
+//     }
+// };
 
-personalMovieDB.start();
+// personalMovieDB.start();
 
-personalMovieDB.detectPersonalLevel();
+// personalMovieDB.detectPersonalLevel();
 
-personalMovieDB.rememberMyFilms();
+// personalMovieDB.rememberMyFilms();
 
-personalMovieDB.showMyDB();
+// personalMovieDB.showMyDB();
 
-personalMovieDB.writeYourGenres();
+// personalMovieDB.writeYourGenres();
 
-personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.toggleVisibleMyDB();
 
-personalMovieDB.showMyDB();
+// personalMovieDB.showMyDB();
 
+// *****
 
 // function getCoupeNumber(a) {
 
@@ -197,3 +190,134 @@ personalMovieDB.showMyDB();
 //     return str;
 // }
 
+
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// };
+
+// function isBudgetEnough(data) {
+//     let square = data.shops.map((item) => item.width * item.length),
+//         sum = 0;
+
+//     for (let i = 0; i < square.length; i++) {
+//         sum += square[i];
+//     }
+//     sum *= data.moneyPer1m3 * data.height;
+    
+//     if (sum < data.budget) {
+//         return "Бюджета достаточно";
+//     } else {
+//         return "Бюджета недостаточно";
+//     }
+// }
+
+// isBudgetEnough(shoppingMallData);
+
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+//     let newArr = [],
+//         empty = [];
+//     arr.sort();
+//     console.log(arr);
+    
+//     for (let i = 0; i < 3; i++) {
+//         let k = 0;
+//         for (let j = i * 3; j < (3 + i * 3); j++) {
+//             empty[k] = arr[j];
+//             k++;
+//         }
+//         newArr[i] = [...empty];
+//     }
+
+//     newArr[3] = "Оставшиеся студенты:";
+//     if (arr.length < 10) {
+//         newArr[3] = "Оставшиеся студенты: -";
+//     } else {
+//         for (let i = 9; i < arr.length; i++) {
+//             newArr[3] += ` ${arr[i]}`;
+//             if (i != arr.length - 1) {
+//                 newArr[3] += `,`;
+//             } 
+//         }
+//     }
+//     return newArr;
+// }
+
+// sortStudentsByGroups(students);
+
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = prop ? 'Открыто' : 'Закрыто';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if ((+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1))) < +average.slice(0, -1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors[0] = {name: 'Mike', age: 32};
+    return copy;
+}
+
+transferWaitors(restorantData);
